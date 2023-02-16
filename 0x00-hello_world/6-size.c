2a3f4da8bad5c138880267a326e3a5f4a8a87d57
Festo1 <festoeducation@gmail.com>
2023-02-16 14:37:35 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,8 +18,8 @@ sizeof(b));
 printf("Size of a long int: %lu bytes(s)\n", (unsigned long)
 sizeof(c));
 printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)
+sizeof(d));
+printf("Size of a float: %lu bytes(s)\n", (unsigned long)
 sizeof(e));
-printf("Size of a char: %lu bytes(s)\n", (unsigned long)
-sizeof(f));
 return (0);
 }
