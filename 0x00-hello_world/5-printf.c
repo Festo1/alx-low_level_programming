eadb83a6497ecf42bc65a85f98449ad8f3b1830d
Festo1 <festoeducation@gmail.com>
2023-02-16 13:47:21 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main -In this programe a new lin will be printed using printif
  * Return: 0 (Which means success)
  */
 int main(void)
 {
-	printif("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
