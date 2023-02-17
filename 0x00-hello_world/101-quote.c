c4d51c873fd6b01c1f4d423e69b80373da729e02
Festo1 <festoeducation@gmail.com>
2023-02-16 17:00:58 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 /**
  * main -A program that will on a new line without
  * using printf or puts functions
- * Return: 1 (Success)
+ * Return: Always 1
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\"
-- Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\"-
+Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
