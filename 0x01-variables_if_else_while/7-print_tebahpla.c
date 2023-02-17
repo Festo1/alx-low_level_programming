0ff5eb93082cf512982835c79692610d211ca400
Festo1 <festoeducation@gmail.com>
2023-02-17 15:43:31 -0800

    6-print_numberz

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /**
- * main - A program that prints the lowercase alphabet in 
+ * main - A program that prints the lowercase alphabet in
  * reverse, followed by a new line.
  * Return: Always 0 (Success)
  */
