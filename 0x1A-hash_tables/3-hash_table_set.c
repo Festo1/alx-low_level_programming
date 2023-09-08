4c200dc2bbb4501a40815088d299b6891bd521f7
Festo1 <festoeducation@gmail.com>
2023-09-08 22:34:25 +0300

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,29 +1,21 @@
 #include "hash_tables.h"
 
 /**
- * create_and_add_node - Malloc, set values, and insert a node into
- * the hash table.
- *
- * Description
- * This function creates a new hash node, sets its key and value,
- * and inserts it into
- * the specified index of the hash table's array.
- *
- * @ht: The hash table where the node will be inserted.
- * @key: The key for the new node (cannot be an empty string).
- * @value: The value associated with the key.
- * @idx: The index in the hash table's array where the node will be inserted.
- *
- * Return: 1 if successful, 0 if it fails.
+ * create_and_add_node - malloc, set values, and insert node into hash table
+ * @ht: hash table
+ * @key: key; can't be empty string
+ * @value: value
+ * @idx: index to insert in at hash table
+ * Return: 1 if success, 0 if fail
  */
 int create_and_add_node(hash_table_t *ht, const char *key, const char *value,
 			unsigned long int idx)
 {
-	hash_node_t *node = NULL; /* Declare a pointer to a hash node. */
+	hash_node_t *node = NULL;
 	char *k;
 	char *v;
 
-	node = malloc(sizeof(hash_node_t)); /* Allocate memory for the new node. */
+	node = malloc(sizeof(hash_node_t));
 	if (!node)
 		return (0);
 
@@ -42,7 +34,6 @@ int create_and_add_node(hash_table_t *ht, const char *key, const char *value,
 		return (0);
 	}
 
-	/* Set the key and value of the new node */
 	node->key = k;
 	node->value = v;
 
@@ -56,32 +47,29 @@ int create_and_add_node(hash_table_t *ht, const char *key, const char *value,
 }
 
 /**
- * hash_table_set - Add an element to a hash table.
- *
- * Description
- * This function adds a key-value pair to the hash table. If the key
- * already exists, it updates the value; otherwise, it creates a new node
- * and inserts it.
- *
- * @ht: The hash table where the key-value pair will be added.
- * @key: The key for the new node (cannot be an empty string).
- * @value: The value associated with the key.
- *
- * Return: 1 if successful, 0 if it fails.
+ * hash_table_set - add element to hash table
+ * @ht: hash table
+ * @key: key; can't be empty string
+ * @value: value
+ * Return: 1 if success, 0 if fail
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	/* get index */
+	/* if key already exists, update value and return */
+	/* else create node */
+	/* set ht idx ptr to node; else add node to front if collision */
+
 	unsigned long int idx;
 	hash_node_t *node = NULL;
 	char *v;
 
-	/* Check for invalid inputs and return 0 if any condition is met.*/
 	if (!ht || !(ht->array) || !key || strlen(key) == 0 || !value)
 		return (0);
 
 	idx = key_index((const unsigned char *)key, ht->size);
 
-	node = (ht->array)[idx]; /*  Traverse the linked list at the index.*/
+	node = (ht->array)[idx];
 	while (node && (strcmp(key, node->key) != 0))
 		node = node->next;
 	if (node != NULL)
@@ -92,8 +80,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (node->value)
 			free(node->value);
 		node->value = v;
+		return (1);
 	}
 
-	/* Create a new node and add it to the hash table. */
 	return (create_and_add_node(ht, key, value, idx));
 }
