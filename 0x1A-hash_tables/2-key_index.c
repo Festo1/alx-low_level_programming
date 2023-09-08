68093004938546aeb36997e7c188d780232e4500
Festo1 <festoeducation@gmail.com>
2023-09-08 21:42:07 +0300

    2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -3,10 +3,6 @@
 /**
  * key_index - gives the index of a key in a hash table array.
  *
- * Description
- * This function calculates the index where a key/value pair should be stored
- * in the hash table's array using the djb2 hash algorithm and the modulo operation.
- *
  * @key: The key for which the index is calculated (a string).
  * @size: The size of the hash table's array.
  * Return: The index where the key/value pair should be stored in the hash table array.
@@ -15,10 +11,12 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int idx;
 
+	/* If the size is 0, return 0 as there's no array to index into. */
 	if (size == 0)
-		return (0); /* If the size is 0, return 0 as there's no array to index into. */
+		return (0);
 
-	idx = hash_djb2(key); /* Calculate the hash value using the djb2 algorithm. */
+	/* Calculate the hash value using the djb2 algorithm. */
+	idx = hash_djb2(key);
 	return (idx % size);
 }
 
