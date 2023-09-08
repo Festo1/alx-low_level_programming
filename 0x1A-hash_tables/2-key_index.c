4d4d925ab538052a9d6ccd0e3dbea38a2cc5cd83
Festo1 <festoeducation@gmail.com>
2023-09-08 21:51:28 +0300

    2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -5,9 +5,19 @@
  *
  * @key: The key for which the index is calculated (a string).
  * @size: The size of the hash table's array.
- * Return: The index where the key/value pair should be stored in the hash table array.
+ * Return: The index where the key/value pair should be
+ * stored in the hash table array.
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	return (hash_djb2(key) % size);
+	unsigned long int idx;
+	
+	/* If the size is 0, return 0 as there's no array to index into. */
+	if (size == 0)
+		return (0);
+
+	/* Calculate the hash value using the djb2 algorithm. */
+	idx = hash_djb2(key);
+	return (idx % size);
 }
+
